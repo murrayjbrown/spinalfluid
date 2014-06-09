7d2dd1a0d0318168e0b387af32f6cb09c86f4420
murrayjbrown <mjb@mjbrown.com>
2014-06-09 17:45:07 -0400

    Enhanced navigation bars, esp. menu bar below header

--- menu-secondary.php
+++ menu-secondary.php
@@ -1,18 +1,15 @@
-<?php if ( has_nav_menu( 'secondary' ) ) {
-    include_once 'inc/navbar-walker.php';
-    wp_nav_menu(
-        array(
-            'theme_location'  => 'secondary',
-            'depth'           => 2,
-            'container'       => 'div',
-            'container_id'    => 'menu-secondary',
-            'container_class' => '',
-            'menu_id'         => '',
-            'menu_class'      => '',
-            'fallback_cb'     => '',
-            'items_wrap'      => '<div class="wrap"><section class="section-container horizontal-navdata-section="horizontal-nav">%3$s</section></div>',
-            'walker'          => new NavBarWalker()
-        )
-    );
+<?php if ( has_nav_menu( 'secondary' ) ) :
+ include_once 'inc/topbar-walker.php';
 
-} ?>
+ wp_nav_menu( array(
+    'container' => 'nav',
+    'theme_location' => 'secondary',
+    'container_class' => 'menu-bar',
+    'menu_class' => '',
+    'menu_id' => 'menu-secondary-items',
+    'items_wrap' => '<section class="top-bar-section"><ul class="left"><li class="divider">%3$s</li></ul></section>',
+    'walker' => new Foundation_Walker(),
+    'fallback_cb' => '' ) );
+
+ endif;
+?>
