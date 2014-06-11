06ac03cc2c8d55028d3aa887b89123879cee865c
murrayjbrown <mjb@mjbrown.com>
2014-06-11 07:21:55 -0400

    Replaced <hr> elements with border styles; tweaked colour scheme.

--- comments-loop-nav.php
+++ comments-loop-nav.php
@@ -1,9 +1,10 @@
 <?php if ( get_option( 'page_comments' ) && 1 < get_comment_pages_count() ) { ?>
 
-	<div class="comments-nav">
-		<?php previous_comments_link( __( '&larr; Previous', 'spinalfluid' ) ); ?>
-		<span class="page-numbers"><?php printf( __( 'Page %1$s of %2$s', 'spinalfluid' ), ( get_query_var( 'cpage' ) ? absint( get_query_var( 'cpage' ) ) : 1 ), get_comment_pages_count() ); ?></span>
-		<?php next_comments_link( __( 'Next &rarr;', 'spinalfluid' ) ); ?><hr>
-	</div><!-- .comments-nav -->
+    <div class="comments-nav">
+        <?php previous_comments_link( __( '&larr; Previous', 'spinalfluid' ) ); ?>
+        <span class="page-numbers"><?php printf( __( 'Page %1$s of %2$s', 'spinalfluid' ), ( get_query_var( 'cpage' ) ? absint( get_query_var( 'cpage' ) ) : 1 ), get_comment_pages_count() ); ?></span>
+        <?php next_comments_link( __( 'Next &rarr;', 'spinalfluid' ) ); ?>
+        <?php // <hr> /* DISABLED (mjbrown) */ ?>
+    </div><!-- .comments-nav -->
 
 <?php } ?>
--- footer.php
+++ footer.php
@@ -1,29 +1,29 @@
-			<?php
-			if(! is_page_template('page-templates/front-page.php')){
-				get_sidebar( 'primary' ); // Loads the sidebar-primary.php template.
-				get_sidebar( 'secondary' ); // Loads the sidebar-secondary.php template.
-			}
-			?>
+            <?php
+            if(! is_page_template('page-templates/front-page.php')){
+                get_sidebar( 'primary' ); // Loads the sidebar-primary.php template.
+                get_sidebar( 'secondary' ); // Loads the sidebar-secondary.php template.
+            }
+            ?>
 
-		</div><!-- #main -->
+        </div><!-- #main -->
 
-		<?php get_sidebar( 'subsidiary' ); // Loads the sidebar-subsidiary.php template. ?>
+        <?php get_sidebar( 'subsidiary' ); // Loads the sidebar-subsidiary.php template. ?>
 
-		<?php get_template_part( 'menu', 'subsidiary' ); // Loads the menu-subsidiary.php template. ?>
+        <?php get_template_part( 'menu', 'subsidiary' ); // Loads the menu-subsidiary.php template. ?>
 
-		<footer id="footer">
+        <footer id="footer">
 
-			<div class="wrap">
-				<hr>
-				<div class="footer-content">
-					<?php echo apply_atomic_shortcode( 'footer_content', '<p class="credit">' . __( 'Copyright &copy; [the-year] [site-link]. Powered by [wp-link] and [theme-link].', 'spinalfluid' ) . '</p>' ); ?>
-				</div><!-- .footer-content -->
+            <div class="wrap">
+                <?php // <hr> /* DISABLED (mjbrown) */ ?>
+                <div class="footer-content">
+                    <?php echo apply_atomic_shortcode( 'footer_content', '<p class="credit">' . __( 'Copyright &copy; [the-year] [site-link]. Powered by [wp-link] and [theme-link].', 'spinalfluid' ) . '</p>' ); ?>
+                </div><!-- .footer-content -->
 
-			</div>
+            </div>
 
-		</footer><!-- #footer -->
-	</div><!-- #container -->
-	<?php wp_footer(); // wp_footer ?>
+        </footer><!-- #footer -->
+    </div><!-- #container -->
+    <?php wp_footer(); // wp_footer ?>
 
 </body>
 </html>
