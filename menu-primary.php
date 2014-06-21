1522bbb2cf5fe9f30f85fd9a10eaa70e93839c7f
murrayjbrown <mjb@mjbrown.com>
2014-06-20 23:40:36 -0400

    Added search bar to topbar; removed redundant menu-bar.

--- menu-primary.php
+++ menu-primary.php
@@ -2,21 +2,29 @@
 include_once 'inc/topbar-walker.php';
 if ( has_nav_menu( 'primary' ) ) :
 
-  //$title = '<ul class="title-area"><li class="name"><h1><a href="' . home_url('/') . '">' . get_bloginfo( 'name' ) . '</a></h1></li><li class="toggle-topbar menu-icon"><a href="#"><span>' . __('Menu','spinalfluid') . '</span></a></li></ul>';
+  $title = '<ul class="title-area">
+    <li class="name"><h1><a href="' . home_url('/') . '">' . get_bloginfo( 'name' ) . '</a></h1>
+    </li><li class="toggle-topbar menu-icon"><a href="#"><span>' . __('Menu','spinalfluid') . '</span></a></li>
+    </ul>';
 
-  $title = '<ul class="title-area"><li class="name"><h1><a href="' . home_url('/') . '">' . get_bloginfo( 'name' ) . '</a></h1></li><li class="toggle-topbar menu-icon"><a href="#"><span>' . __('Menu','spinalfluid') . '</span></a></li></ul>';
+  $search = '<ul class="search-area">
+      <li class="has-form">
+        <form method="get" class="search-form" id="topbar-search-form" action="' . home_url('/') . '">
+          <input type="text" name="s" placeholder="Search">
+        </form>
+      </li>
+    </ul>';
+
   echo '<nav class="top-bar" data-topbar>';
-
   wp_nav_menu( array(
     'container' => '',
     'theme_location' => 'primary',
     'container_class' => '',
     'menu_class' => '',
     'menu_id' => 'menu-primary-items',
-    'items_wrap' => $title . '<section class="top-bar-section"><ul class="right"><li class="divider">%3$s</li></ul></section>',
+    'items_wrap' => $title . '<section class="top-bar-section"><ul class="right"><li>' . $search . '</li><li class="divider">%3$s</li></ul></section>',
     'walker' => new Foundation_Walker(),
     'fallback_cb' => '' ) );
-
   echo '</nav>';
 
 endif;
--- menu-secondary.php
+++ menu-secondary.php
@@ -1,15 +1,25 @@
 <?php if ( has_nav_menu( 'secondary' ) ) :
  include_once 'inc/topbar-walker.php';
 
+  $search = '<ul class="search-area">
+      <li class="has-form">
+        <form method="get" class="search-form" id="topbar-search-form" action="' . home_url('/') . '">
+          <input type="text" name="s" placeholder="Search">
+        </form>
+      </li>
+    </ul>';
+
  wp_nav_menu( array(
     'container' => 'nav',
     'theme_location' => 'secondary',
-    'container_class' => 'menu-bar',
+    'container_class' => 'top-bar',
     'menu_class' => '',
     'menu_id' => 'menu-secondary-items',
-    'items_wrap' => '<section class="top-bar-section"><ul class="left"><li class="divider">%3$s</li></ul></section>',
+    'items_wrap' => '<section class="top-bar-section"><ul class="left"><li class="divider">%3$s</li></ul><ul class="right"<li>' . $search . '</li></ul></section>',
     'walker' => new Foundation_Walker(),
     'fallback_cb' => '' ) );
 
  endif;
+
+
 ?>
