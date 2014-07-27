4cc39415a48eefb2ba7c220ef024e84f72b979c4
murrayjbrown <mjb@mjbrown.com>
2014-07-27 16:15:27 -0400

    Disabled broken getarchives_*() filters

--- functions.php
+++ functions.php
@@ -455,7 +455,7 @@ function spine2_add_featured_img_class( $img_html ) {
     return $img_html;
 }
 
-add_filter('getarchives_where', 'wse95776_archives_by_cat', 10, 2 );
+//add_filter('getarchives_where', 'wse95776_archives_by_cat', 10, 2 );
 
 /**
  * Filter the posts by category slug
@@ -468,7 +468,7 @@ function wse95776_archives_by_cat($where, $r) {
     return "WHERE wp_posts.post_type = 'post' AND wp_posts.post_status = 'publish' AND wp_terms.slug = 'Uncategorized' AND wp_term_taxonomy.taxonomy = 'category'";
 }
 
-add_filter('getarchives_join', 'wse95776_archives_join',10,2);
+//add_filter('getarchives_join', 'wse95776_archives_join',10,2);
 
 /**
  * Defines the necessary joins to query the terms
